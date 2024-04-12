b69796b1de5698ccf9d6b2e30c60f291bab3dd3e
Fredray-21 <dabadie.frederic@gmail.com>
2024-04-12 19:11:17 +0200

    feat : ajout de la section de droite && clock && debut rotation title

---
+++ V3-glass/js/clock.js
@@ -0,0 +1,12 @@
+export const updateHour = () => {
+    const dateNow = new Date();
+    const hour = { s: dateNow.getSeconds(), m: dateNow.getMinutes(), h: dateNow.getHours() };
+    Object.keys(hour).forEach(key => {
+        const elem = document.getElementById(`clock-${key}`);
+        elem.textContent = hour[key] < 10 ? `0${hour[key]}` : hour[key];
+    });
+}
+
+const location = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const locationElem = document.getElementById('clock-location');
+locationElem.textContent = location;
--- V3-glass/js/main.js
+++ V3-glass/js/main.js
@@ -1,5 +1,38 @@
 import { animateGradients } from './background.js';
+import { updateHour } from './clock.js';
 
 window.addEventListener('load', () => {
+    fetch('https://api.github.com/repos/Fredray-21/Portfolio/commits/main')
+        .then(response => response.json())
+        .then(data => {
+            const elem = document.getElementById('commitHash')
+            elem.textContent = data.sha.slice(0, 7) + '...';
+            elem.href = data.html_url;
+        })
+        .catch(error => console.error('Erreur lors de la requête API :', error));
+
+    const allCards = document.querySelectorAll('.cards .card');
+    allCards.forEach(card => {
+        card.addEventListener('mouseenter', () => {
+            card.classList.add('cardSelected');
+
+            allCards.forEach(cardOfAll => {
+                if (card !== cardOfAll) {
+                    cardOfAll.classList.remove('cardSelected');
+                }
+            });
+        });
+        card.addEventListener('mouseleave', () => {
+            card.classList.remove('cardSelected');
+        });
+    });
+
+
+    
     animateGradients();
+    updateHour();
+    setInterval(() => {
+        updateHour();
+    }, 1000);
 });
+
