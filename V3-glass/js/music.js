b98eb1fb9f9bf4de5137483833aa4b64837f87ab
Fredray-21 <dabadie.frederic@gmail.com>
2024-04-13 19:37:07 +0200

    feat : style page 404

--- V3-glass/js/music.js
+++ V3-glass/js/music.js
@@ -1,6 +1,6 @@
 let spotifyPreviewVariable = null;
 
-fetch('https://api.stats.fm/api/v1/users/ptfred2104/streams/')
+fetch('https://api.stats.fm/api/v1/users/ptfred2104/streams/recent')
 .then(response => response.json())
 .then(data => {
     const {albums, artists, name, spotifyPreview} = data.items[0].track;
