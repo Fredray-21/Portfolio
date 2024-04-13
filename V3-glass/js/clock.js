91d542ce33295d971647835fac2668e068646d39
Fredray-21 <dabadie.frederic@gmail.com>
2024-04-13 19:55:49 +0200

    fix(js): gestion des if si le DOM existe pas

--- V3-glass/js/clock.js
+++ V3-glass/js/clock.js
@@ -3,10 +3,16 @@ export const updateHour = () => {
     const hour = { s: dateNow.getSeconds(), m: dateNow.getMinutes(), h: dateNow.getHours() };
     Object.keys(hour).forEach(key => {
         const elem = document.getElementById(`clock-${key}`);
-        elem.textContent = hour[key] < 10 ? `0${hour[key]}` : hour[key];
+        if(elem)
+        {
+            elem.textContent = hour[key] < 10 ? `0${hour[key]}` : hour[key];
+        }
     });
 }
 
 const location = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const locationElem = document.getElementById('clock-location');
-locationElem.textContent = location;
+if(locationElem)
+{
+    locationElem.textContent = location;
+}
--- V3-glass/js/music.js
+++ V3-glass/js/music.js
@@ -1,41 +1,42 @@
 let spotifyPreviewVariable = null;
 
 fetch('https://api.stats.fm/api/v1/users/ptfred2104/streams/recent')
-.then(response => response.json())
-.then(data => {
-    const {albums, artists, name, spotifyPreview} = data.items[0].track;
+    .then(response => response.json())
+    .then(data => {
+        const { albums, artists, name, spotifyPreview } = data.items[0].track;
 
-    const musicTitleElem = document.getElementById('musicTitle');
-    const musicArtistElem = document.getElementById('musicArtist');
-    const musicAlbumElem = document.getElementById('musicAlbum');
-    const musicDiskElem = document.getElementById('disk');
+        const musicTitleElem = document.getElementById('musicTitle');
+        const musicArtistElem = document.getElementById('musicArtist');
+        const musicAlbumElem = document.getElementById('musicAlbum');
+        const musicDiskElem = document.getElementById('disk');
 
-    musicTitleElem.textContent = name;
-    musicArtistElem.textContent = "Of: "+ [...new Set(artists.map(artist => artist.name))].join(', ');
-    musicAlbumElem.textContent = "Album: "+albums[0].name;
-    musicAlbumElem.title = albums[0].name;
+        musicTitleElem.textContent = name;
+        musicArtistElem.textContent = "Of: " + [...new Set(artists.map(artist => artist.name))].join(', ');
+        musicAlbumElem.textContent = "Album: " + albums[0].name;
+        musicAlbumElem.title = albums[0].name;
 
-    musicDiskElem.style.backgroundImage = `url('${albums[0].image}')`;
+        musicDiskElem.style.backgroundImage = `url('${albums[0].image}')`;
 
-    spotifyPreviewVariable = spotifyPreview;
-})
-.catch(error => console.error('Erreur lors de la requête API :', error));
+        spotifyPreviewVariable = spotifyPreview;
+    })
+    .catch(error => console.error('Erreur lors de la requête API :', error));
 
 const containerDiskElem = document.getElementById('containerDisk');
 const musicDiskElem = document.getElementById('disk');
+if (containerDiskElem) {
+    const audio = new Audio();
+    containerDiskElem.addEventListener("click", () => {
+        musicDiskElem.classList.toggle('diskAction');
 
-const audio = new Audio();
-containerDiskElem.addEventListener("click", () => {
-    musicDiskElem.classList.toggle('diskAction');
-
-    if(musicDiskElem.classList.contains('diskAction') && spotifyPreviewVariable !== null){
-        audio.src = spotifyPreviewVariable;
-        audio.play();
-        audio.onended = () => {
-            musicDiskElem.classList.remove('diskAction');
+        if (musicDiskElem.classList.contains('diskAction') && spotifyPreviewVariable !== null) {
+            audio.src = spotifyPreviewVariable;
+            audio.play();
+            audio.onended = () => {
+                musicDiskElem.classList.remove('diskAction');
+            }
         }
-    }
-    else{
-        audio.pause();
-    }
-});
+        else {
+            audio.pause();
+        }
+    });
+}
